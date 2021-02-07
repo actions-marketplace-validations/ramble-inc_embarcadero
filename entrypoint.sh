df2b4464f3618e700cda3c6f4d54139a9ba8c1d7
mizozobu <hamc.m.so.j12@gmail.com>
2021-02-08 00:45:11 +0900

    feat: json mapping

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,17 @@ echo "Arriving at Embarcadero..."
 
 AUTHOR_USERNAME="$1"
 AUTHOR_EMAIL="$2"
-SRC_FILES="$3"
+MAPPINGS="$3"
 DEST_USERNAME="$4"
 DEST_REPO="$5"
-DEST_DIR="$6"
-DEST_BRANCH=${7:-main}
-CREATE_PR=${8:-false}
-DEST_PR_BASE_BRANCH=${9:-main}
+DEST_BRANCH=${6:-main}
+CREATE_PR=${7:-false}
+DEST_PR_BASE_BRANCH=${8:-main}
 
 # input validation
 if [ -z "$AUTHOR_USERNAME" ]
 then
-  echo '"author-email" must be defined'
+  echo '"author-username" must be defined'
   return -1
 fi
 if [ -z "$AUTHOR_EMAIL" ]
@@ -23,9 +22,9 @@ then
   echo 'author-email" must be defined'
   return -1
 fi
-if [ -z "$SRC_FILES" ]
+if [ -z "$MAPPINGS" ]
 then
-  echo 'src-files" must be defined'
+  echo 'mappings" must be defined'
   return -1
 fi
 if [ -z "$DEST_USERNAME" ]
@@ -36,11 +35,6 @@ fi
 if [ -z "$DEST_REPO" ]
 then
   echo 'dest-repo" must be defined'
-  return -1
-fi
-if [ -z "$DEST_DIR" ]
-then
-  echo 'dest-dir" must be defined'
   return -1
 fi
 
@@ -55,15 +49,22 @@ git clone --single-branch "https://$ACCESS_TOKEN@github.com/$DEST_USERNAME/$DEST
 cd "$DIST_REPO_DIR"
 git checkout -b $DEST_BRANCH
 
-echo "Copying files to $DEST_USERNAME/$DEST_REPO..."
-mkdir -p $DIST_REPO_DIR/$DEST_DIR
-cp -r "$SRC_REPO_DIR/$SRC_FILES" "$DIST_REPO_DIR/$DEST_DIR"
+for JSON in $(echo $MAPPINGS | jq -c .[])
+do
+  SRC=$(echo $JSON | jq -r .src)
+  DEST=$(echo $JSON | jq -r .dest)
+  echo "Copying $SRC_REPO_DIR/$SRC to $DIST_REPO_DIR/$DEST..."
+  mkdir -p $DIST_REPO_DIR/$DEST
+  cp -r $SRC_REPO_DIR/$SRC $DIST_REPO_DIR/$DEST
+done
 
+git add -N .
+CHANGED_FILES=$(git diff --name-only)
 git add .
 git status
 
 # avoid failing with "nothing to commit, working tree clean"
-if (git diff-index --quiet HEAD)
+if [ -z "$CHANGED_FILES" ]
 then
   echo "nothing to commit, working tree clean"
   return 0
@@ -75,12 +76,16 @@ git commit -m "Update from $COMMIT_URL"
 echo "Pushing to $DEST_USERNAME/$DEST_REPO@$DEST_BRANCH..."
 git push -u origin "HEAD:$DEST_BRANCH"
 
-if ($CREATE_PR)
+if "$CREATE_PR"
 then
   echo "Creating a PR $DEST_PR_BASE_BRANCH <- $DEST_BRANCH..."
 
   TITLE="[Embarcadero]: Update from $DEST_USERNAME/$DEST_REPO"
-  BODY="## Base Commit\n$COMMIT_URL\n\n## Files\n- $DEST_DIR"
+  BODY="## Base Commit\n$COMMIT_URL\n\n## Files\n"
+  for FILE in $CHANGED_FILES
+  do
+    BODY="${BODY}\n- [$FILE](https://github.com/$DEST_USERNAME/$DEST_REPO/blob/$DEST_BRANCH/$FILE)"
+  done
 
   PR_URL=$(
     curl --fail \
@@ -90,7 +95,7 @@ then
       https://api.github.com/repos/$DEST_USERNAME/$DEST_REPO/pulls \
       -d "{\"base\":\"$DEST_PR_BASE_BRANCH\",\"head\":\"$DEST_BRANCH\",\"title\":\"$TITLE\",\"body\":\"$BODY\"}" \
     | tr -d "\n" \
-    | jq .html_url
+    | jq -r .html_url
   )
 
   if [ "$PR_URL" == "null" ]
